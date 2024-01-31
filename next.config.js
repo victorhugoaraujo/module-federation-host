e88970fc158aa5924b6d4c87509e3aedb9b47e6a
Victor  Araújo <victor_araujo@gap.com>
2024-01-31 13:37:14 -0300

    remove next experiments

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: (config, options) => {
     const { isServer } = options;
-    config.experiments = { topLevelAwait: true };
+    // config.experiments = { topLevelAwait: true };
     config.plugins.push(
       new NextFederationPlugin({
         name: 'main',
